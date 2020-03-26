765bb636c53213ced9826eab7250855ad299b235
Elton Fonseca <elton869@gmail.com>
2020-03-26 09:48:13 -0300

    41 - Adicionando o autoload ao projeto

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once "uteis/formatador.php";
-require_once "dados/cursos.php";
-require_once "exibicao/principal.php";
+require_once "autoload/autoload.php";
+
+// require_once "uteis/formatador.php";
+// require_once "dados/cursos.php";
+// require_once "exibicao/principal.php";
 
